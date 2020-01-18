f4b1c27e5b59e25ef81090b96393ed59e08cf40e
maidieho <mhostudy18@gmail.com>
2020-01-18 16:37:43 +0800

    added PlaceOrderController, edited OrderCart View Index

---
+++ Controllers/PlaceOrderController.cs
@@ -0,0 +1,62 @@
+﻿using CafeO.Models;
+using System;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace CafeO.Controllers
+{
+    public class PlaceOrderController : Controller
+    {
+        ApplicationDbContext db = new ApplicationDbContext();
+
+
+        //
+        // GET: /PlaceOrder/Confirm
+        public ActionResult Confirm()
+        {
+            return View();
+        }
+
+        //
+        // POST: /PlaceOrder/ConfirmOrder
+        [HttpPost]
+        public ActionResult Confirm(FormCollection values)
+        {
+            var order = new Order();
+            UpdateModel(order);
+
+            order.Username = User.Identity.Name;
+            order.OrderDate = DateTime.Now;
+
+            //Save Order
+            db.Orders.Add(order);
+            db.SaveChanges();
+
+            //Process the order
+            var cart = OrderCart.GetCart(this.HttpContext);
+            cart.CreateOrder(order);
+
+            return RedirectToAction("Complete", new { id = order.Id });
+
+        }
+
+        //
+        // GET: /PlaceOrder/Complete
+        public ActionResult Complete(int id)
+        {
+            // Validate customer owns this order
+            bool isValid = db.Orders.Any(
+                o => o.Id == id &&
+                     o.Username == User.Identity.Name);
+
+            if (isValid)
+            {
+                return View(id);
+            }
+            else
+            {
+                return View("Error");
+            }
+        }
+    }
+}
