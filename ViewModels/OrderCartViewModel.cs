2fd2c44af3d2664b4d9245a81337a7f3b5565aad
maidieho <mhostudy18@gmail.com>
2020-01-18 15:59:35 +0800

    added ViewModels & Views for OrderCart edited OrderCartController

---
+++ ViewModels/OrderCartViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using CafeO.Models;
+
+namespace CafeO.ViewModels
+{
+    public class OrderCartViewModel
+    {
+        public List<CartItem> CartItems { get; set; }
+        public decimal CartTotal { get; set; }
+    }
+}
