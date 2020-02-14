8f1df0a2eb15bb1e3ded2a4f1e14341a7d702248
maidieho <mhostudy18@gmail.com>
2020-02-14 13:08:37 +0800

    removed xml format from Global.asax.cs

--- Global.asax.cs
+++ Global.asax.cs
@@ -19,10 +19,6 @@ namespace CafeO
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
- //           GlobalConfiguration.Configuration.Formatters.JsonFormatter.SerializerSettings
- //               .ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore;
- //           GlobalConfiguration.Configuration.Formatters
- //               .Remove(GlobalConfiguration.Configuration.Formatters.XmlFormatter);
         }
     }
 }
